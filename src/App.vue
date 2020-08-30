278dbb5f735d9a603646e9681d176a52903de7e5
shampurrs <edwardmiller604@gmail.com>
2020-08-30 14:57:42 +1200

    Installed dependencies and created the naviagtion bar styles and layout

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Navigation />
+  </div>
+</template>
+
+<script>
+import Navigation from "./components/Nav.vue";
+export default {
+  components: {
+    Navigation
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="h-20 shadow-2xl">
+    <div class="flex justify-between items-center py-3">
+      <div class="mx-10">
+        <input
+          class="w-64 h-10 px-3 py-1 rounded-full bg-gray-200 text-lg focus:outline-none"
+          placeholder="Search..."
+        />
+        <div class="flex">
+          <i class="fas fa-search"></i>
+        </div>
+      </div>
+      <div class="mx-10">
+        <span class="text-gray-600 text-4xl">ChampionDex</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+::root {
+  background: #f7fafc;
+}
+::placeholder {
+  color: #718096;
+  opacity: 1;
+}
+
+.fas {
+  position: absolute;
+  color: #0275d8;
+  top: 30px;
+  left: 265px;
+}
+
+input {
+  transition: 0.2s ease;
+}
+
+input:focus {
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+}
+</style>
